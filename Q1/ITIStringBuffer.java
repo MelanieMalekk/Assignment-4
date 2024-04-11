67c5daa6cda4e7b0d45c25469f7339ff69983b97
James Malek <jammalek@hotmail.com>
2024-04-10 23:40:22 -0400

    finished up some stuff

--- Q1/ITIStringBuffer.java
+++ Q1/ITIStringBuffer.java
@@ -1,20 +1,60 @@
 public class ITIStringBuffer {
     SinglyLinkedList<String> bufferList;
-    public ITIStringBuffer() {
-        //throw new UnsupportedOperationException( "replace with your implementation" );
-        bufferList = new SinglyLinkedList<String>();
-    }
+    private int lengthholder;
+
+    // public ITIStringBuffer() {
+    //     //throw new UnsupportedOperationException( "replace with your implementation" );
+    //     bufferList = new SinglyLinkedList<String>();
+    // }
+
+
 
     public ITIStringBuffer(String  firstString){
-        throw new UnsupportedOperationException( "replace with your implementation" );
+       // throw new UnsupportedOperationException( "replace with your implementation" );
+       if(firstString == null){
+        throw new IllegalArgumentException("the string is null");
+    }
+    bufferList = new SinglyLinkedList<String>();
+    bufferList.addFirst(firstString);
+    lengthholder += firstString.length();
+    
     }
 
     public void append(String nextString){
         throw new UnsupportedOperationException( "replace with your implementation" );
+        // if(firstString == null){
+        //     throw new IllegalArgumentException("the string is null");
+        // }
+        // mylist = new SinglyLinkedList<String>();
+        // mylist.addFirst(firstString);
+        // lengthholder += firstString.length();
     }
 
     public String toString(){
         throw new UnsupportedOperationException( "replace with your implementation" );
-    }
+        int counter = 0;
+        char[] mychar_holder;
+        char[] mychar_final;
 
+        mychar_final = new char[lengthholder];
+
+
+
+        for(String holder : mylist){
+
+            mychar_holder = holder.toCharArray();
+
+            for(int i = 0; i < (mychar_holder).length; i++){
+                mychar_final[counter] = (mychar_holder[i]);
+                counter++;
+
+            }
+
+        }
+        String myholder = new String(mychar_final);
+
+        return myholder;
+    }
 }
+
+
